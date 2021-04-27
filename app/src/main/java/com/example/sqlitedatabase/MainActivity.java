9aff46dfe05daad2cd2bf53b7426b65b60c00a80
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-04-27 22:05:30 +0530

    select data

--- app/src/main/java/com/example/sqlitedatabase/MainActivity.java
+++ app/src/main/java/com/example/sqlitedatabase/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.sqlitedatabase;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,7 +13,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     DatabaseHelper mydb;
     EditText editname,editsurname,editmarks;
-    Button BtnAdd;
+    Button BtnAdd,BtnView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +27,33 @@ public class MainActivity extends AppCompatActivity {
         editmarks=findViewById(R.id.marks);
 
         BtnAdd=findViewById(R.id.add);
+        BtnView=findViewById(R.id.view);
         AddData();
 
+    }
+    public void ViewAll()
+    {
+        BtnView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Cursor res=mydb.getAllData();
+               if (res.getCount()==0)
+               {
+                   showMessage("Error","No Data Found");
+                    return;
+               }
+               StringBuffer buffer=new StringBuffer();
+               while (res.moveToNext()){
+                   buffer.append("ID :"+res.getString(0)+"\n");
+                   buffer.append("NAME :"+res.getString(1)+"\n");
+                   buffer.append("SURNAME :"+res.getString(2)+"\n");
+                   buffer.append("MARKS :"+res.getString(3)+"\n\n");
+               }
+
+               //show all data
+                showMessage("Data",buffer.toString());
+            }
+        });
     }
     public void  AddData()
     {
@@ -37,10 +64,20 @@ public class MainActivity extends AppCompatActivity {
 
                if (inserted==true){
                    Toast.makeText(MainActivity.this,"Data Inserted",Toast.LENGTH_LONG);
+
                }else{
                    Toast.makeText(MainActivity.this,"Data not Inserted",Toast.LENGTH_SHORT);
                }
             }
         });
     }
+
+    public void showMessage(String title,String Message){
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
+    }
+
 }
