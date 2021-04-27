5c60232f1322027f4369e1fa6e8df77e702f1d67
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-04-27 14:56:44 +0530

    created sqlite database

--- app/src/main/java/com/example/sqlitedatabase/MainActivity.java
+++ app/src/main/java/com/example/sqlitedatabase/MainActivity.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    DatabaseHelper mydb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mydb=new DatabaseHelper(this);
     }
 }
